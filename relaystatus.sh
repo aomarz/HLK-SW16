5ace47d373029cebfcd89ee08d2b6a75731d7923
aomarz <65833846+aomarz@users.noreply.github.com>
2020-06-11 13:22:44 -0400

    Hola cómo está el cambió
    En el modulo HLK sw16 v2 eso es el resultado que me sale en el mío y unas preguntas en el tuyo ya funciona con Google Home

--- relaystatus.sh
+++ relaystatus.sh
@@ -15,8 +15,8 @@ SYNTAX="Usage: $0 HOST PORT\n\
 where\tHOST := hostname/IP address of the HLK-SW16\
 \n\tPORT := port number of the HLK-SW16 (generally: 8080)\
 \n\n
-\texample#1: $0 10.10.10.199 8080\n\
-\t(Get each relay status of HLK-SW16 located at 10.10.10.199 port 8080)\n\
+\texample#1: $0 192.168.16.254:8080\n\
+\t(Get each relay status of HLK-SW16 located at 192.168.16.254 port 8080)\n\
 \t\n[script by ram@pluslab.com, if you like this script please drop a note]\n\
 "
 
@@ -31,8 +31,8 @@ fi
 
 
 OUTPUT=`(echo -e "$PREFIX$PREFIX2$SWITCHID$SWITCHTO$SUFFIX";sleep 1)| ncat $1 $2` 
-#OUTPUT=`ncat 10.10.97.199 8080 -c "bash -c \"echo -e '$PREFIX$PREFIX2$SWITCHID$SWITCHTO$SUFFIX';sleep 1\""`
-#another try ncat 10.10.97.199 8080 -c "bash -c '(echo hello;sleep 3;read -n100 -d\"\\0x00\" abc;echo $abc)'"
+#OUTPUT=`ncat 192.168.16.254:8080 -c "bash -c \"echo -e '$PREFIX$PREFIX2$SWITCHID$SWITCHTO$SUFFIX';sleep 1\""`
+#another try ncat 192.168.16.254:8080 -c "bash -c '(echo hello;sleep 3;read -n100 -d\"\\0x00\" abc;echo $abc)'"
 USABLE=`echo $OUTPUT | od -A n -v -t x1 | tr -d '\n'|sed 's/.*cc 0c/cc 0c/'|sed 's/ dd .*/ dd/'`
 counter=0
 for byte in $USABLE; do
